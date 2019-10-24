44ef79ec04829d72e40003a029bbfe08ce597954
Marianne Piquet <mariannexpiquet@gmail.com>
2019-10-24 18:09:24 +0200

    organisation des exercices - reprise à l'exo 2

--- index.js
+++ index.js
@@ -1,55 +1,68 @@
 "use strict"
 
 
-// CONSIGNE
-// Créer une fonction qui appelle l'API de Swapi 
-// Elle retourne Yoda 
-
-
-// // Je teste la récupération des personnages (avec fetch())
-// function getPeople() {
-//     return fetch("https://swapi.co/api/people/")
-//     .then(res => res.json())
-//     .then(people => console.log(people.results))
-// }
-// getPeople(); // OK
-
-/////////// TEST //////////////
-// AXIOS : récupération de l'objet People
-const getPeople = () => {
-  axios.get('https://swapi.co/api/people/')
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-}
-getPeople();
-/////////// TEST //////////////
-
-
-// Essai avec autre syntaxe
+/*==========================================================================================
+Exercice 1
+Dans votre fichier index.js, créez une fonction qui appelle l'API de SWAPI et qui retourne Yoda
+===========================================================================================*/
 
 async function getYoda() {
   let noYoda = true;
   let i = 1;
+  const yodaId = document.getElementById("yoda");
 
   while (noYoda) {
     await axios
       .get(`https://swapi.co/api/people/?page=${i}`)
-      .then((response) => {        
+      .then((response) => {
         const characters = response.data.results;
         const filteredCharacters = characters.filter(person => person.name === "Yoda");
         if (filteredCharacters.length > 0) {
           noYoda = false;
           console.log(filteredCharacters[0].name);
+          const yoda = filteredCharacters[0].name
+          yodaId.innerHTML = `Sage comme ${yoda} un jour, tu seras`
         }
-          i++;
+        i++;
       })
   }
 
 }
+/*==========================================================================================
+Exercice 2
+Créez une fonction qui retourne le nom de chaque espèce qui contient au moins 2 personnages
+===========================================================================================*/
+// Il me faut toutes les espèces. Donc il va falloir boucler sur toutes les pages. Je déclare donc une variable compteur i = 1
+// Mon action se passera donc dans la boucle 
+async function getSpecies() {
+  let i = 1;
+  while(noSpecies){
+    await axios
+      .get("https://swapi.co/api/species/")
+      .then((response) => {
+        // Action
+      })
+
+  }
+}
+
+/*==========================================================================================
+Exercice 3
+Créez une fonction qui retourne la somme des tailles (oui je sais, çà n'a aucun sens) de tous les humains
+===========================================================================================*/
+
+
+/*==========================================================================================
+Exercice 4
+Créez une fonction qui retourne un tableau de tous les tous les humains, contenant:
+- le nom, 
+- la taille, 
+- le poids, 
+- le nom des films dans lesquels ils sont apparus,
+- le nom de leur planète d'origine
+===========================================================================================*/
+
+// ===================== RECREATION =========================\\
 
 //////// Test d'une petit fonctionnalité /////////
 
@@ -59,42 +72,40 @@ async function getCharacter() {
   const searchedCharacter = document.getElementById("characterName").value;
   const characterDesc = document.getElementById("characterDesc");
 
-  while (noCharacter) {
+  
     try {
-      await axios
-      .get(`https://swapi.co/api/people/?page=${i}`)
-      .then((response) => {        
-        const characters = response.data.results;
-        const filteredCharacters = characters.filter(person => {
-          // on utilise return quand l'instruction n'est pas sur une seule ligne 
-          return person.name === searchedCharacter;
-        });
-        ///Si mon personnage a bien été trouvé : 
-        if (filteredCharacters.length > 0) {
-          noCharacter = false; // Dès qu'il trouve le perso, ça passe a false, donc on sort de la boucle
-          const character = filteredCharacters[0];
-          characterDesc.innerHTML = `${character.name} est né(e) en ${character.birth_year}`;
-        } 
-        
-        i++;
+      while (noCharacter) {
+        await axios
+          .get(`https://swapi.co/api/people/?page=${i}`)
+          .then((response) => {
+            const characters = response.data.results;
+            const filteredCharacters = characters.filter(person => {
+              // on utilise return quand l'instruction n'est pas sur une seule ligne 
+              return person.name === searchedCharacter;
+            });
+            ///Si mon personnage a bien été trouvé : 
+            if (filteredCharacters.length > 0) {
+              noCharacter = false; // Dès qu'il trouve le perso, ça passe a false, donc on sort de la boucle
+              const character = filteredCharacters[0];
+              characterDesc.innerHTML = `${character.name} est né(e) en ${character.birth_year}`;
+            }
 
-      })
-      .catch(() => {
-        throw new Exception();
-      })
-      /* 
-      Quand on fait un bloc try / Catch, pour indiquer qu'il y a eu une erreur quelque part, 
-      on fait un throw new Exception() pour "raise" (soulever) une erreur que l'on peut "catcher" pour
-      mettre l'erreur dans le catch d'après.
-      */
+            i++;
+
+          })
+          .catch(() => {
+            throw new Exception();
+          })
+        /* 
+        Quand on fait un bloc try / Catch, pour indiquer qu'il y a eu une erreur quelque part, 
+        on fait un throw new Exception() pour "raise" (soulever) une erreur que l'on peut "catcher" pour
+        mettre l'erreur dans le catch d'après.
+        */
+      }
 
     } catch (error) {
       characterDesc.innerHTML = `Votre personnage ${searchedCharacter} n'existe pas.`;
     }
-    
+
   }
-
-}
-
-
 
